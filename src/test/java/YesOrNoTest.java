dfd15e3ccee72a2a6b3786f524c43b818d68fd50
Nejert <nejer777@gmail.com>
2023-05-29 16:59:49 +0600

    add project under version control

---
+++ src/test/java/YesOrNoTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class YesOrNoTest {
+    @Test
+    public void testBoolToWord() {
+        assertEquals(YesOrNo.boolToWord(true),"Yes");
+        assertEquals(YesOrNo.boolToWord(false),"No");
+    }
+}
